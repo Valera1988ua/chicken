fc6cb94e4aebb4ecfc16e8386e56df163774ac33
Valeriy <108978943+Valera1988ua@users.noreply.github.com>
2023-04-24 15:32:27 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -3598,6 +3598,9 @@
                 document.documentElement.classList.add(className);
             }));
         }
+        function uniqArray(array) {
+            return array.filter(((item, index, self) => self.indexOf(item) === index));
+        }
         function FLS(message) {
             setTimeout((() => {
                 if (window.FLS) console.log(message);
@@ -3769,6 +3772,100 @@
             bodyUnlock();
             document.documentElement.classList.remove("menu-open");
         }
+        class ScrollWatcher {
+            constructor(props) {
+                let defaultConfig = {
+                    logging: true
+                };
+                this.config = Object.assign(defaultConfig, props);
+                this.observer;
+                !document.documentElement.classList.contains("watcher") ? this.scrollWatcherRun() : null;
+            }
+            scrollWatcherUpdate() {
+                this.scrollWatcherRun();
+            }
+            scrollWatcherRun() {
+                document.documentElement.classList.add("watcher");
+                this.scrollWatcherConstructor(document.querySelectorAll("[data-watch]"));
+            }
+            scrollWatcherConstructor(items) {
+                if (items.length) {
+                    this.scrollWatcherLogging(`Прокинувся, стежу за об'єктами (${items.length})...`);
+                    let uniqParams = uniqArray(Array.from(items).map((function(item) {
+                        return `${item.dataset.watchRoot ? item.dataset.watchRoot : null}|${item.dataset.watchMargin ? item.dataset.watchMargin : "0px"}|${item.dataset.watchThreshold ? item.dataset.watchThreshold : 0}`;
+                    })));
+                    uniqParams.forEach((uniqParam => {
+                        let uniqParamArray = uniqParam.split("|");
+                        let paramsWatch = {
+                            root: uniqParamArray[0],
+                            margin: uniqParamArray[1],
+                            threshold: uniqParamArray[2]
+                        };
+                        let groupItems = Array.from(items).filter((function(item) {
+                            let watchRoot = item.dataset.watchRoot ? item.dataset.watchRoot : null;
+                            let watchMargin = item.dataset.watchMargin ? item.dataset.watchMargin : "0px";
+                            let watchThreshold = item.dataset.watchThreshold ? item.dataset.watchThreshold : 0;
+                            if (String(watchRoot) === paramsWatch.root && String(watchMargin) === paramsWatch.margin && String(watchThreshold) === paramsWatch.threshold) return item;
+                        }));
+                        let configWatcher = this.getScrollWatcherConfig(paramsWatch);
+                        this.scrollWatcherInit(groupItems, configWatcher);
+                    }));
+                } else this.scrollWatcherLogging("Сплю, немає об'єктів для стеження. ZzzZZzz");
+            }
+            getScrollWatcherConfig(paramsWatch) {
+                let configWatcher = {};
+                if (document.querySelector(paramsWatch.root)) configWatcher.root = document.querySelector(paramsWatch.root); else if ("null" !== paramsWatch.root) this.scrollWatcherLogging(`Эмм... батьківського об'єкта ${paramsWatch.root} немає на сторінці`);
+                configWatcher.rootMargin = paramsWatch.margin;
+                if (paramsWatch.margin.indexOf("px") < 0 && paramsWatch.margin.indexOf("%") < 0) {
+                    this.scrollWatcherLogging(`йой, налаштування data-watch-margin потрібно задавати в PX або %`);
+                    return;
+                }
+                if ("prx" === paramsWatch.threshold) {
+                    paramsWatch.threshold = [];
+                    for (let i = 0; i <= 1; i += .005) paramsWatch.threshold.push(i);
+                } else paramsWatch.threshold = paramsWatch.threshold.split(",");
+                configWatcher.threshold = paramsWatch.threshold;
+                return configWatcher;
+            }
+            scrollWatcherCreate(configWatcher) {
+                this.observer = new IntersectionObserver(((entries, observer) => {
+                    entries.forEach((entry => {
+                        this.scrollWatcherCallback(entry, observer);
+                    }));
+                }), configWatcher);
+            }
+            scrollWatcherInit(items, configWatcher) {
+                this.scrollWatcherCreate(configWatcher);
+                items.forEach((item => this.observer.observe(item)));
+            }
+            scrollWatcherIntersecting(entry, targetElement) {
+                if (entry.isIntersecting) {
+                    !targetElement.classList.contains("_watcher-view") ? targetElement.classList.add("_watcher-view") : null;
+                    this.scrollWatcherLogging(`Я бачу ${targetElement.classList}, додав клас _watcher-view`);
+                } else {
+                    targetElement.classList.contains("_watcher-view") ? targetElement.classList.remove("_watcher-view") : null;
+                    this.scrollWatcherLogging(`Я не бачу ${targetElement.classList}, прибрав клас _watcher-view`);
+                }
+            }
+            scrollWatcherOff(targetElement, observer) {
+                observer.unobserve(targetElement);
+                this.scrollWatcherLogging(`Я перестав стежити за ${targetElement.classList}`);
+            }
+            scrollWatcherLogging(message) {
+                this.config.logging ? FLS(`[Спостерігач]: ${message}`) : null;
+            }
+            scrollWatcherCallback(entry, observer) {
+                const targetElement = entry.target;
+                this.scrollWatcherIntersecting(entry, targetElement);
+                targetElement.hasAttribute("data-watch-once") && entry.isIntersecting ? this.scrollWatcherOff(targetElement, observer) : null;
+                document.dispatchEvent(new CustomEvent("watcherCallback", {
+                    detail: {
+                        entry
+                    }
+                }));
+            }
+        }
+        projectModules.watcher = new ScrollWatcher({});
         const productFile = "json/products.json";
         const recipesFile = "json/recipes.json";
         const newsFile = "json/news_and_actions.json";
@@ -6975,11 +7072,12 @@
         window.addEventListener("load", (() => {
             if (recipesPageBlock) getRecipes();
         }));
-        const targetElement = document.querySelector(".page__recipes") || document.querySelector(".recipes__items");
-        if (recipesBlock && targetElement) window.addEventListener("scroll", (() => {
-            const topTargetElement = targetElement.getBoundingClientRect().top;
-            if (topTargetElement <= window.innerHeight / 2 && !addRecipes) {
-                getRecipes();
+        window.addEventListener("scroll", (() => {
+            const targetElement = document.querySelector(".page__recipes") || document.querySelector(".recipes__items");
+            if (targetElement.classList.contains("_watcher-view") && !addRecipes) {
+                setTimeout((() => {
+                    getRecipes();
+                }), 500);
                 addRecipes = true;
                 preloaderRecipes.remove();
             }
@@ -7663,13 +7761,12 @@
         }));
         window.addEventListener("scroll", (() => {
             const targetElement = document.querySelector(".page__products");
-            if (productsBlock && targetElement) {
-                const topTargetElement = targetElement.getBoundingClientRect().top;
-                if (topTargetElement <= window.innerHeight / 2 && !productAdd) {
+            if (targetElement.classList.contains("_watcher-view") && !productAdd) {
+                setTimeout((() => {
                     getProducts();
-                    productAdd = true;
-                    preloaderProducts.remove();
-                }
+                }), 500);
+                productAdd = true;
+                preloaderProducts.remove();
             }
         }));
         async function getProducts() {
@@ -8643,11 +8740,12 @@
                 if (newsDate && newsContent) preloaderNews.remove();
             }
         }));
-        const news_targetElement = document.querySelector(".page__news");
-        if (newsBlock && news_targetElement) window.addEventListener("scroll", (() => {
-            const topTargetElement = news_targetElement.getBoundingClientRect().top;
-            if (topTargetElement <= window.innerHeight / 2 && !newsAdd) {
-                getNews();
+        window.addEventListener("scroll", (() => {
+            const targetElement = document.querySelector(".page__news");
+            if (targetElement.classList.contains("_watcher-view") && !newsAdd) {
+                setTimeout((() => {
+                    getNews();
+                }), 500);
                 newsAdd = true;
                 preloaderNews.remove();
             }
